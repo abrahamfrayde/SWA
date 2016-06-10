e11a514a69d71b72dacb7309b3f3cdee3b8512f4
abrahamfrayde <abraham.frayde@gmail.com>
2016-06-10 13:58:34 -0500

    Estructura general del Proyecto

---
+++ SWAMID/Bibliotecas/Sistema/Conexion/Conexion.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace Conexion
+{
+    public class Conexion
+    {
+        public SqlConnection _connection;
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                //if(_connection == null)
+                //{
+                _connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Conexion"].ConnectionString);
+                //}
+                return _connection;
+            }
+        }
+
+        public SqlConnection AllConnection(string conn)
+        {
+
+            //if (_connection == null)
+            //{
+            _connection = new SqlConnection(ConfigurationManager.ConnectionStrings[conn].ConnectionString);
+            //}
+            return _connection;
+
+        }
+        public void OpenConnection()
+        {
+            if (_connection.State == ConnectionState.Closed || _connection.State == ConnectionState.Broken)
+            {
+                _connection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (_connection.State == ConnectionState.Open)
+            {
+                _connection.Close();
+            }
+        }
+        public void Dispose()
+        {
+            // Close connection
+        }
+    }
+}
---
+++ SWAMID/WebSite/Beneficiarios.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Beneficiarios.aspx.cs" Inherits="Administracion_Beneficiarios" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+</asp:Content>
+
