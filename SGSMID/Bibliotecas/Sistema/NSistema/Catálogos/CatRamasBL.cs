06b19d907f83350a48ce152bc1a4a89ca59ed4f3
tony130675 <tony130675@outlook.com>
2016-06-24 11:11:50 -0500

    Capa BL de Ramas y TiposSolicitudes

--- SGSMID/Bibliotecas/Sistema/NSistema/Catálogos/CatRamasBL.cs
+++ SGSMID/Bibliotecas/Sistema/NSistema/Catálogos/CatRamasBL.cs
@@ -3,10 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using DSistema;
+using ESistema;
+using ESistema.Catalogos;
 
 namespace NSistema.Catálogos
 {
-    class CatRamasBL
+    public class CatRamasBL
     {
+
+        // Método para insertar ramas BL
+        public int InsertarRamas(CatRamas _CatRamas)
+        {
+            // Bloque para manejos de errores 
+            try
+            {
+                // Definición y creación de la instancia DAL del tipo CatRamasDAL
+                CatRamasDAL DAL = new CatRamasDAL();
+                return DAL.InsertarRamas(_CatRamas);
+            }
+            // Manejo de las excepciones de bloque try
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        // Método para modificar ramas BL
+        public int ModificarRamas(CatRamas _CatRamas)
+        {
+            // Bloque para manejos de errores
+            try
+            {
+                // Definición y creación de la instancia DAL del tipo CatRamasDAL
+                CatRamasDAL DAL = new CatRamasDAL();
+                return DAL.ModificarRamas(_CatRamas);
+            }
+            // Manejo de las excepciones de bloque try
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        // Método para eliminar ramas BL
+        public int EliminarRamas(CatRamas _CatRamas)
+        {
+            // Bloque para manejos de errores
+            try
+            {
+                // Definición y creación de la instancia DAL del tipo CatRamasDAL
+                CatRamasDAL DAL = new CatRamasDAL();
+                return DAL.EliminarRamas(_CatRamas);
+            }
+            // Manejo de las excepciones de bloque try
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        // Método para obtener ramas BL
+        public List<CatRamas> ObtenerRamas(int _id = 0, string _filtro = null)
+        {
+            // Bloque para manejos de errores
+            try
+            {
+                // Definición y creación de la instancia DAL del tipo CatRamasDAL
+                CatRamasDAL DAL = new CatRamasDAL();
+                return DAL.ObtenerRamas(_id, _filtro);
+            }
+            // Manejo de las excepciones de bloque try
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+
     }
 }
--- SGSMID/Bibliotecas/Sistema/NSistema/Catálogos/CatTiposSolicitudBL.cs
+++ SGSMID/Bibliotecas/Sistema/NSistema/Catálogos/CatTiposSolicitudBL.cs
@@ -3,10 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using DSistema;
+using ESistema;
+using ESistema.Catalogos;
 
 namespace NSistema.Catálogos
 {
-    class CatTiposSolicitudBL
+    // Método para insertar tipos de solicitudes BL
+    public class CatTiposSolicitudBL
     {
+
+        public int InsertarTiposSolicitud(CatTiposSolicitud _CatTiposSolicitud)
+        {
+            // Bloque para manejos de errores
+            try
+            {
+                // Definición y creación de la instancia DAL del tipo CatTiposSolicitud
+                CatTiposSolicitudDAL DAL = new CatTiposSolicitudDAL();
+                return DAL.InsertarTiposSolicitud(_CatTiposSolicitud);
+            }
+            // Manejo de las excepciones de bloque try
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        // Método para modificar tipos de solicitudes BL
+        public int ModificarTiposSolicitud(CatTiposSolicitud _CatTiposSolicitud)
+        {
+            // Bloque para manejos de errores
+            try
+            {
+                // Definición y creación de la instancia DAL del tipo CatTiposSolicitud
+                CatTiposSolicitudDAL DAL = new CatTiposSolicitudDAL();
+                return DAL.ModificarTiposSolicitud(_CatTiposSolicitud);
+            }
+            // Manejo de las excepciones de bloque try
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        // Método para eliminar tipos de solicitudes BL
+        public int EliminarTiposSolicitud(CatTiposSolicitud _CatTiposSolicitud)
+        {
+            // Bloque para manejos de errores
+            try
+            {
+                // Definición y creación de la instancia DAL del tipo CatTiposSolicitud
+                CatTiposSolicitudDAL DAL = new CatTiposSolicitudDAL();
+                return DAL.EliminarTiposSolicitud(_CatTiposSolicitud);
+            }
+            // Manejo de las excepciones de bloque try
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        // Método para obtener tipos de solicitudes BL
+        public List<CatTiposSolicitud> ObtenerTiposSolicitud(int _id = 0, string _filtro = null)
+        {
+            // Bloque para manejos de errores
+            try
+            {
+                // Definición y creación de la instancia DAL del tipo CatTiposSolicitud
+                CatTiposSolicitudDAL DAL = new CatTiposSolicitudDAL();
+                return DAL.ObtenerTiposSolicitud(_id, _filtro);
+            }
+            // Manejo de las excepciones de bloque try
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+
     }
 }
