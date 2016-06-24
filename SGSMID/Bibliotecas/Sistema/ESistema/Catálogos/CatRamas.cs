ed1a34fbcbbd3e7befd55375d6ed814213d0f9b9
coordinador <coordinador@CUL-LAP-CIBER01>
2016-06-24 08:46:31 -0500

    Capa Entidades de CatRamas y CatTiposSolicitudes

--- SGSMID/Bibliotecas/Sistema/ESistema/Catálogos/CatRamas.cs
+++ SGSMID/Bibliotecas/Sistema/ESistema/Catálogos/CatRamas.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace ESistema.Catalogos
 {
-    class CatRamas
+    /// <summary>
+    /// Clase CatRamas que hace referencia a la tabla tbl_cat_ramas de la base de datos BD_SGS
+    /// </summary>
+    public class CatRamas
     {
+        // Propiedades de la clase CatRamas
+        public int iIdRama { get; set; }
+        public string cNombre { get; set; }
+        public CentroCostos ObjCentroCostos { get; set; }
+        public Usuarios ObjUsuarioGestion { get; set; }
+        public DateTime dtFechaRegistro { get; set; }
+        public Boolean bActivo { get; set; }
+
+        // Constructor de la clase CatRamas
+        public CatRamas() { }
     }
 }
--- SGSMID/Bibliotecas/Sistema/ESistema/Catálogos/CatTiposSolicitud.cs
+++ SGSMID/Bibliotecas/Sistema/ESistema/Catálogos/CatTiposSolicitud.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace ESistema.Catalogos
 {
-    class CatTiposSolicitud
+    /// <summary>
+    /// Clase CatTiposSolicitud que hace referencia a la tabla tbl_cat_tipossolicitud de la base de datos BD_SGS
+    /// </summary>
+    public class CatTiposSolicitud
     {
+        // Propiedades de la clase CatTiposSolicitud
+        public int iIdTipoSolicitud { get; set; }
+        public string cNombre { get; set; }
+        public CatRamas ObjRamas { get; set; }
+        public CatProcesos ObjProcesos { get; set; }
+        public Usuarios ObjUsuarioGestion { get; set; }
+        public DateTime dtFechaRegistro { get; set; }
+        public Boolean bActivo { get; set; }
+
+        // Constructor de la clase CatTiposSolicitud
+        public CatTiposSolicitud() { }
     }
 }
